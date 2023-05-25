759f94bcbb1a98339b682bf1bd7c0c2b73fcf5e3
Mahir Kadić <kadic.mahir.pro@gmail.com>
2023-05-25 16:41:08 +0200

    Adding swagger local url

--- apps/nextjs/src/pages/docs/index.tsx
+++ apps/nextjs/src/pages/docs/index.tsx
@@ -4,14 +4,14 @@ import "swagger-ui-react/swagger-ui.css";
 
 const SwaggerUI = dynamic(() => import("swagger-ui-react"), { ssr: false });
 
-// const IS_SERVER = typeof window === 'undefined'
-// const origin = IS_SERVER
-//   ? process.env.NEXT_PUBLIC_API_URL
-//   : window.location.origin
+const IS_SERVER = typeof window === "undefined";
+const origin = IS_SERVER
+  ? process.env.NEXT_PUBLIC_API_URL
+  : window.location.origin;
 
 const Api: NextPage = () => {
   // Serve Swagger UI with our OpenAPI schema
-  return <SwaggerUI url="/api/openapi.json" />;
+  return <SwaggerUI url={`${origin}/api/openapi.json`} />;
 };
 
 export default Api;
