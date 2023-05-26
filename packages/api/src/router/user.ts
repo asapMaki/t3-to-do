61c4bfcea3747e497b718df44d4fa7c1dca070dc
Mahir Kadić <kadic.mahir.pro@gmail.com>
2023-05-26 09:01:09 +0200

    Fixing ts

--- packages/api/src/router/user.ts
+++ packages/api/src/router/user.ts
@@ -1,23 +1,22 @@
 import { UserSchema } from "./../../../db/prisma/generated/zod/modelSchema/UserSchema";
 import { router, publicProcedure } from "../trpc";
 import { z } from "zod";
-import { protectedProcedure } from "../trpc";
 
 export const userRouter = router({
-  // all: publicProcedure
-  //   .meta({
-  //     openapi: {
-  //       method: "GET",
-  //       path: "/users",
-  //       tags: ["user"],
-  //       summary: "Get all users",
-  //     },
-  //   })
-  //   .input(z.void())
-  //   .output(z.array(UserSchema))
-  //   .query(({ ctx }) => {
-  //     return ctx.prisma.user.findMany();
-  //   }),
+  all: publicProcedure
+    .meta({
+      openapi: {
+        method: "GET",
+        path: "/users",
+        tags: ["user"],
+        summary: "Get all users",
+      },
+    })
+    .input(z.void())
+    .output(z.array(UserSchema))
+    .query(({ ctx }) => {
+      return ctx.prisma.user.findMany();
+    }),
   // create: protectedProcedure
   //   .input(UserSchema)
   //   .output(UserSchema)
